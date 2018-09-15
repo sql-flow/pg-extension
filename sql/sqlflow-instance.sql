fdb76f048b396d7457f41cbf48817b70397150bc
Christophe CHAUVET <christophe.chauvet@gmail.com>
2018-09-15 12:03:42 +0200

    Add eventlog table

--- sql/sqlflow-instance.sql
+++ sql/sqlflow-instance.sql
@@ -69,6 +69,21 @@ from
 	 }'::json as col1, 1 as col2 ) x
 ORDER BY 1
 */
+
+CREATE TABLE sqlflow.instance_eventlog
+(
+	id bigserial,
+	instance_id bigint REFERENCES sqlflow.instance ON DELETE CASCADE ON UPDATE CASCADE,
+	event_time timestamp with time zone NOT NULL DEFAULT now(),
+	description text NOT NULL,
+	CONSTRAINT instance_eventlog_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.instance_eventlog
+    OWNER to sqlflow;
 	
 --
 -- Add comments on tables and columns
@@ -85,3 +100,5 @@ COMMENT ON TABLE sqlflow.instance_transition IS 'Workflow instance transition';
 COMMENT ON COLUMN sqlflow.instance_transition.instance_id IS 'Link to one instance';
 COMMENT ON COLUMN sqlflow.instance_transition.flow_state IS 'State of this instance transition';
 COMMENT ON COLUMN sqlflow.instance_transition.conditions IS 'States of each conditions related on transition';
+
+COMMENT ON TABLE sqlflow.instance_eventlog IS 'Trace events on each instance';

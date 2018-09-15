3496b0c5d643c6c80dfafff05db50bb5695e8adf
Christophe CHAUVET <christophe.chauvet@gmail.com>
2018-09-15 10:42:59 +0200

    Add instance activity

--- sql/sqlflow-instance.sql
+++ sql/sqlflow-instance.sql
@@ -8,7 +8,7 @@ SET search_path TO sqlflow, public;
 CREATE TABLE sqlflow.instance
 (
     id bigserial,
-	workflow_id integer REFERENCES sqlflow.workflow ON DELETE CASCADE,
+	-- workflow_id integer REFERENCES sqlflow.workflow ON DELETE CASCADE,
 	version_id integer REFERENCES sqlflow.version ON DELETE CASCADE,
     rel_table character varying(127) NOT NULL,
 	flow_type flow_type NOT NULL DEFAULT 'row',
@@ -21,3 +21,30 @@ WITH (
 
 ALTER TABLE sqlflow.instance
     OWNER to sqlflow;
+	
+CREATE TABLE sqlflow.instance_activity
+(
+	id bigserial,
+	instance_id bigint REFERENCES sqlflow.instance ON DELETE CASCADE,
+	flow_state flow_state NOT NULL DEFAULT 'init',
+    CONSTRAINT instance_activity_pkey PRIMARY KEY (id)
+	
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.instance_activity
+    OWNER to sqlflow;
+	
+	
+--
+-- Add comments on tables and columns
+--	
+COMMENT ON TABLE sqlflow.instance IS 'Workflow instance declaration';
+COMMENT ON COLUMN sqlflow.instance.title IS 'Workflow instance title';
+
+COMMENT ON TABLE sqlflow.instance_activity IS 'Workflow instance activity';
+COMMENT ON COLUMN sqlflow.instance_activity.instance_id IS 'Link to one instance';
+COMMENT ON COLUMN sqlflow.instance_activity.flow_state IS 'State of this instance activity';
+

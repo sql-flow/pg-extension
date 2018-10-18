21abb2cc9d6664adb5edae379a364d7df63041fc
Christophe CHAUVET <christophe.chauvet@gmail.com>
2018-10-18 12:59:02 +0200

    Add structure for workflow support
    Add default sqlflow schema with tables, functions, type inside
    Add dedicate user for all sqlflow object
    
    Ref #2

---
+++ sql/sqlflow-instance.sql
@@ -0,0 +1,103 @@
+--
+-- SQLFlow extension
+-- Workflow engine in SQL
+--
+
+SET search_path TO sqlflow, public;
+
+CREATE TABLE sqlflow.instance
+(
+    id bigserial,
+    -- workflow_id integer REFERENCES sqlflow.workflow ON DELETE CASCADE,
+    version_id integer REFERENCES sqlflow.version ON DELETE CASCADE ON UPDATE CASCADE,
+    rel_table character varying(127) NOT NULL,
+    flow_type flow_type NOT NULL DEFAULT 'row',
+    flow_state flow_state NOT NULL DEFAULT 'init',
+    CONSTRAINT instance_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.instance
+    OWNER to sqlflow;
+ 
+CREATE TABLE sqlflow.instance_activity
+(
+    id bigserial,
+    instance_id bigint REFERENCES sqlflow.instance ON DELETE CASCADE ON UPDATE CASCADE,
+    flow_state flow_state NOT NULL DEFAULT 'init',
+    tasks json DEFAULT '{}',
+    CONSTRAINT instance_activity_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.instance_activity
+    OWNER to sqlflow;
+
+CREATE TABLE sqlflow.instance_transition
+(
+    id bigserial,
+    instance_id bigint REFERENCES sqlflow.instance ON DELETE CASCADE ON UPDATE CASCADE,
+    flow_state flow_state NOT NULL DEFAULT 'init',
+    conditions json DEFAULT '{}',
+    CONSTRAINT instance_transition_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.instance_transition
+    OWNER to sqlflow;
+        
+/*
+--
+-- Query to retrieve tasks (instance_activity) 
+-- and conditions (instance_transition)
+--
+select json_object_keys(col1) as condition_id,
+       (col1->json_object_keys(col1)->'id')::text::int as cond_id, 
+       (col1->json_object_keys(col1)->'state')::text as cond_state,
+           (col1->json_object_keys(col1)->'result')::text as cond_result
+from
+        (select '{"1": {"id": 1512, "state": "complete", "result": true}, 
+                  "2": {"id": 1623, "state": "waiting", "result": false},
+                  "3": {"id": 5467, "state": "progress", "result": false}
+         }'::json as col1, 1 as col2 ) x
+ORDER BY 1
+*/
+
+CREATE TABLE sqlflow.instance_eventlog
+(
+    id bigserial,
+    instance_id bigint REFERENCES sqlflow.instance ON DELETE CASCADE ON UPDATE CASCADE,
+    event_time timestamp with time zone NOT NULL DEFAULT now(),
+    description text NOT NULL,
+    CONSTRAINT instance_eventlog_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.instance_eventlog
+    OWNER to sqlflow;
+        
+--
+-- Add comments on tables and columns
+--      
+COMMENT ON TABLE sqlflow.instance IS 'Workflow instance declaration';
+COMMENT ON COLUMN sqlflow.instance.version_id IS 'Workflow instance link version';
+
+COMMENT ON TABLE sqlflow.instance_activity IS 'Workflow instance activity';
+COMMENT ON COLUMN sqlflow.instance_activity.instance_id IS 'Link to one instance';
+COMMENT ON COLUMN sqlflow.instance_activity.flow_state IS 'State of this instance activity';
+COMMENT ON COLUMN sqlflow.instance_activity.tasks IS 'States of each task related on activity';
+
+COMMENT ON TABLE sqlflow.instance_transition IS 'Workflow instance transition';
+COMMENT ON COLUMN sqlflow.instance_transition.instance_id IS 'Link to one instance';
+COMMENT ON COLUMN sqlflow.instance_transition.flow_state IS 'State of this instance transition';
+COMMENT ON COLUMN sqlflow.instance_transition.conditions IS 'States of each conditions related on transition';
+
+COMMENT ON TABLE sqlflow.instance_eventlog IS 'Trace events on each instance';
---
+++ sql/sqlflow-structure.sql
@@ -0,0 +1,183 @@
+--
+-- SQLFlow extension
+-- Workflow engine in SQL
+--
+
+--
+-- Add user to manage sqlflow object s in database
+--
+CREATE ROLE sqlflow WITH
+  NOLOGIN
+  NOSUPERUSER
+  INHERIT
+  NOCREATEDB
+  NOCREATEROLE
+  NOREPLICATION;
+
+COMMENT ON ROLE sqlflow IS 'User for sqlflow extension';
+
+--
+-- Add specific schema for SQLFlow
+--
+CREATE SCHEMA sqlflow
+    AUTHORIZATION sqlflow;
+
+COMMENT ON SCHEMA sqlflow
+    IS 'SQLFlow Engine';
+
+SET search_path TO sqlflow, public;
+--
+-- Add types
+--
+CREATE TYPE flow_type AS ENUM ('row', 'statement');
+CREATE TYPE flow_cond AS ENUM ('and', 'or', 'xor');
+CREATE TYPE flow_state AS ENUM ('init', 'progress', 'complete', 'cancelled', 'waiting');
+
+--
+-- Workflow structure tables
+--
+CREATE TABLE sqlflow.workflow
+(
+    id serial,
+    uref uuid NOT NULL,
+    title character varying(64) NOT NULL,
+    rel_table character varying(127) NOT NULL,
+    flow_type flow_type NOT NULL DEFAULT 'row',
+    CONSTRAINT workflow_pkey PRIMARY KEY (id),
+    CONSTRAINT workflow_ref_uniq UNIQUE (uref)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.workflow
+    OWNER to sqlflow;
+
+CREATE TABLE sqlflow.version
+(
+    id serial,
+    uref uuid NOT NULL,
+    title character varying(64) NOT NULL,
+    workflow_id integer REFERENCES sqlflow.workflow ON DELETE CASCADE,
+    revision integer NOT NULL default 1,
+    CONSTRAINT version_pkey PRIMARY KEY (id),
+    CONSTRAINT version_ref_uniq UNIQUE (uref),
+    CONSTRAINT version_revision_uniq UNIQUE (workflow_id, revision)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.version
+    OWNER to sqlflow;
+
+CREATE TABLE sqlflow.activity
+(
+    id serial,
+    uref uuid NOT NULL,
+    title character varying(64) NOT NULL,
+    version_id integer REFERENCES sqlflow.version ON DELETE CASCADE,
+    logic_in flow_cond NOT NULL DEFAULT 'xor',
+    logic_out flow_cond NOT NULL DEFAULT 'xor',
+    flow_start boolean NOT NULL DEFAULT false,
+    flow_stop boolean NOT NULL DEFAULT false,
+    CONSTRAINT activity_pkey PRIMARY KEY (id),
+    CONSTRAINT activity_ref_uniq UNIQUE (uref)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.activity
+    OWNER to sqlflow;
+
+
+CREATE TABLE sqlflow.transition
+(
+    id serial,
+    uref uuid NOT NULL,
+    title character varying(64) NOT NULL,
+    act_from_id integer REFERENCES sqlflow.activity ON DELETE CASCADE,
+    act_to_id integer REFERENCES sqlflow.activity ON DELETE CASCADE,
+    trans_cond text NOT NULL DEFAULT 'true',
+    CONSTRAINT transition_pkey PRIMARY KEY (id),
+    CONSTRAINT transition_ref_uniq UNIQUE (uref)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.transition
+    OWNER to sqlflow;
+
+
+CREATE TABLE sqlflow.task
+(
+    id serial,
+    uref uuid NOT NULL,
+    title character varying(64) NOT NULL,
+    activity_id integer REFERENCES sqlflow.activity ON DELETE CASCADE,
+    task_seq integer NOT NULL DEFAULT 1,
+    task_code text NOT NULL,
+    CONSTRAINT task_pkey PRIMARY KEY (id),
+    CONSTRAINT task_ref_uniq UNIQUE (uref)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.task
+    OWNER to sqlflow;
+
+CREATE TABLE sqlflow.condition
+(
+    id serial,
+    uref uuid NOT NULL,
+    title character varying(64) NOT NULL,
+    transition_id integer REFERENCES sqlflow.transition ON DELETE CASCADE,
+    cond_seq integer NOT NULL DEFAULT 1,
+    cond_func character varying(128) NOT NULL,
+    CONSTRAINT condition_pkey PRIMARY KEY (id),
+    CONSTRAINT condition_ref_uniq UNIQUE (uref)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE sqlflow.condition
+    OWNER to sqlflow;
+--
+-- Add comments on tables and columns
+--
+COMMENT ON TABLE sqlflow.workflow IS 'Workflow process declaration';
+COMMENT ON COLUMN sqlflow.workflow.title IS 'Workflow title';
+
+COMMENT ON TABLE sqlflow.version IS 'Versionning workflow process';
+COMMENT ON COLUMN sqlflow.version.title IS 'Version title';
+COMMENT ON COLUMN sqlflow.version.uref IS 'Unique reference, use on import/export';
+COMMENT ON COLUMN sqlflow.version.revision IS 'Incremental workflow revision';
+
+COMMENT ON TABLE sqlflow.activity IS 'Activity on workflow';
+COMMENT ON COLUMN sqlflow.activity.title IS 'Activity title';
+COMMENT ON COLUMN sqlflow.activity.uref IS 'Unique reference, use on import/export';
+COMMENT ON COLUMN sqlflow.activity.version_id IS 'Activity link to version';
+COMMENT ON COLUMN sqlflow.activity.logic_in IS 'Operator logic use when enter to this activity';
+COMMENT ON COLUMN sqlflow.activity.logic_out IS 'Operator logic use when exit to this activity';
+COMMENT ON COLUMN sqlflow.activity.flow_start IS 'Is a start activity (unique per version)';
+COMMENT ON COLUMN sqlflow.activity.flow_stop IS 'Is a end activity (multiple per version)';
+
+COMMENT ON TABLE sqlflow.transition IS 'Transition is link between 2 activities';
+COMMENT ON COLUMN sqlflow.transition.title IS 'Activity title';
+COMMENT ON COLUMN sqlflow.transition.title IS 'Transition title';
+
+COMMENT ON TABLE sqlflow.task IS 'Activity task on workflow';
+COMMENT ON COLUMN sqlflow.task.title IS 'Activity task title';
+COMMENT ON COLUMN sqlflow.task.uref IS 'Unique reference, use on import/export';
+COMMENT ON COLUMN sqlflow.task.task_seq IS 'Task sequence on activity';
+COMMENT ON COLUMN sqlflow.task.task_code IS 'PLpgSQL code execute on this task';
+
+COMMENT ON TABLE sqlflow.condition IS 'Transition condition on workflow';
+COMMENT ON COLUMN sqlflow.condition.title IS 'Condition title';
+COMMENT ON COLUMN sqlflow.condition.uref IS 'Unique reference, use on import/export';
+COMMENT ON COLUMN sqlflow.condition.cond_seq IS 'Order to execute condition on transition';
+COMMENT ON COLUMN sqlflow.condition.cond_func IS 'Function to execute on this condition';

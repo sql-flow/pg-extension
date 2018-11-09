6076c7322c08a0f4648a8212d82a9046617d5e00
Christophe CHAUVET <christophe.chauvet@gmail.com>
2018-11-09 23:36:05 +0100

    Add new tests

--- test/base.sql
+++ test/base.sql
@@ -1,6 +1,6 @@
 -- Start transaction and plan the tests.
 BEGIN;
-SELECT plan(6);
+SELECT plan(10);
 
 -- Run test on sqlflow structure
 SELECT has_schema('sqlflow');
@@ -10,7 +10,11 @@ SELECT has_type('sqlflow', 'flow_cond');
 SELECT has_type('sqlflow', 'flow_state');
 
 SELECT has_table('sqlflow', 'workflow');
-SELECT has_column('sqlflow', 'workflow', 'id', 'id columns exists')
+SELECT has_column('sqlflow', 'workflow', 'id', 'id column exists');
+SELECT has_column('sqlflow', 'workflow', 'uref', 'uref column exists');
+SELECT has_column('sqlflow', 'workflow', 'title', 'title column exists');
+SELECT has_column('sqlflow', 'workflow', 'rel_table', 'rel_table column exists');
+SELECT has_column('sqlflow', 'workflow', 'flow_type', 'flow_type column exists');
 
 -- Finish the tests and clean up.
 SELECT * FROM finish();
